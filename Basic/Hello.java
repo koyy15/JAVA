369d13b641e0afba622fa25a5485a6ff167cd8e8
koyy15 <Syisxn@gmail.com>
2022-10-21 11:04:48 +0700

    First Code
    Helloworlg
    My name is koy
    5555555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Helloworlg!");
+        System.out.println("My name is koy");
+        System.out.println("5555555!");
     }
 }
